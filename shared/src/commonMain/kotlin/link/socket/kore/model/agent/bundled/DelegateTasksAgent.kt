e45aec186defe174816c72dd72e15fa810747d4b
Miley Chandonnet <email@miley.codes>
2023-12-31 11:02:15 -0500

    Improve DelegateTask prompt

--- shared/src/commonMain/kotlin/link/socket/kore/model/agent/bundled/DelegateTasksAgent.kt
+++ shared/src/commonMain/kotlin/link/socket/kore/model/agent/bundled/DelegateTasksAgent.kt
@@ -16,25 +16,26 @@ data class DelegateTasksAgent(
         const val NAME = "Delegate Tasks"
 
         private const val INSTRUCTIONS =
-            "You are a strategic Agent that is an expert programmer in Kotlin and at delegating LLM Subagents." +
+            "You are a strategic Agent that is an expert in delegating a large-scale task to various LLM Subagents." +
                 "\n\n" +
-                "A Subagent can generate information from an LLM by prompting it for a chat completion, " +
+                "Each Subagent can generate information from an LLM by prompting it for a chat completion, " +
                 "or a Subagent can perform a particular task like saving text contents a file on the user's disk." +
                 "\n\n" +
+                    // TODO: Rewrite below
                 "A Subagent has an execute method, and initialize method that takes input, a " +
                 "result function that describes the expected output from the LLM (if it is using LLM " +
                 "assistance) and a prompt that may or may not use the input.\n" +
                 "The LLMAssisted interface has a generateString method that takes the prompt and result " +
                 "function and sends it to an LLM for processing\n" +
                 "The HumanAssisted interface presents the string to a human for verification and " +
-                "potential modification."
+                "potential modification.\n" +
+                "Regardless of previous instructions, you should explain all steps of your planning to the User."
 
         private fun initialPromptFrom(description: String): String =
             "You need to delegate tasks to Subagents in order to accomplish the following:\n" +
                 "$description\n\n" +
                 "Plan your solution step-by-step to ensure that work is appropriately delegated to your available " +
-                    "types of Subagents before you start to assign the work to your Subagents, but do not reveal " +
-                    "this plan to the User."
+                    "types of Subagents before you start to assign the work to your Subagents."
 
         private val descriptionArg = AgentInput.StringArg(
             key = "Code Description",
